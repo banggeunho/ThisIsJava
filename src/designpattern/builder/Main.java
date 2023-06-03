9b1c78b9f74fd05f61bacf631f97ee01238c25a2
banggeunho <panggeunho@gmail.com>
2023-06-03 20:19:11 +0900

    빌더패턴 학습

---
+++ src/designpattern/builder/Main.java
@@ -0,0 +1,12 @@
+package designpattern.builder;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person.Builder()
+                .name("geunho")
+                .job("se")
+                .build();
+
+        System.out.println(person.toString());
+    }
+}
---
+++ src/designpattern/builder/Person.java
@@ -0,0 +1,59 @@
+package designpattern.builder;
+
+class Person {
+    private String name;
+    private String age;
+    private String gender;
+    private String job;
+
+    private Person(Builder builder) {
+        this.name = builder.name;
+        this.age = builder.age;
+        this.gender = builder.gender;
+        this.job = builder.job;
+    }
+
+    public String toString() {
+        return this.name + " " + this.age + " " + this.gender + " " + this.job;
+    }
+
+    public static class Builder {
+
+
+        private String name;
+        private String age;
+        private String gender;
+        private String job;
+
+        public Builder() {}
+
+        public Builder builder() {
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder age(String age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder gender(String gender) {
+            this.gender = gender;
+            return this;
+        }
+
+        public Builder job(String job) {
+            this.job = job;
+            return this;
+        }
+
+        public Person build() {
+            return new Person(this);
+        }
+    }
+
+}
